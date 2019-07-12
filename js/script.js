4d831b622153de75e17d2da57f4d2be60e25353a
Anastasiia Zhmurko <zhana141281@gmail.com>
2019-07-12 11:50:24 +0300

    12/06/19

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+// // -----------switching  menu-tabs in block "Our Services"-----------------
+const tabs = document.querySelectorAll('.link_ourServ_menu a');
+debugger;
+console.log(tabs);
+
+
+// const tabs = document.querySelectorAll('.link_ourServ_menu');
+// console.log(tabs);
+// const ul = document.querySelector('.menu_OurServ_item');
+// console.log(ul);
+// const content = document.querySelectorAll('.art_OurServ');
+// console.log(content);
+
+
+// content.forEach(function (elem) {
+//     elem.hidden = true;
+// });
+//
+// let activeIndex = 0;
+//
+// content[activeIndex].hidden = false;
+// tabs[activeIndex].classList.add('active');
+//
+// ul.addEventListener('click', function (event) {
+//     event.target.classList.add('active');
+//     let atr = event.target.getAttribute('data-content');
+//
+//     for (let i = 0; i <= tabs.length; i++) {
+//         if (tabs[i].dataset.content !== atr) {
+//             tabs[i].classList.remove('active');
+//             content[i].hidden = true;
+//         } else {
+//             content[i].hidden = false;
+//             activeIndex = i;
+//
+//         }
+//     }
+// });
